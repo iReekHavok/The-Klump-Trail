50d3f946cd3d4f3dba68edeaf80ea2af7da5cc93
Brendan Cagampang <37314477+iReekHavok@users.noreply.github.com>
2018-12-02 19:19:04 -0600

    Add files via upload

---
+++ TheKlumpTrail.java
@@ -0,0 +1,42 @@
+/*
+                        The Klump Trail
+Created by Brendan Cagampang, Andrea Ecarma, Edgar Flores, and Mike Palermo.
+
+These were the tasks assigned:
+Brendan - 
+Andrea - 
+Edgar - 
+Mike - 
+
+The purpose of this project is to make Klump laugh. That is all.
+
+This is a clicking game, so the user just needs to click Next and sometimes select an option.
+
+
+*/
+
+package theklumptrail;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+class KlumpFrame{}
+class SitPan{}
+
+public class TheKlumpTrail {
+    public static void main(String[] args) {
+        
+    }
+}
